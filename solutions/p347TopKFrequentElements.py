d207257bd835aaf11b70bda4d839fa2049df628e
Yasser Elsayed <yasser.ebrahim@gmail.com>
2016-05-04 17:02:28 -0700

    removed unnecessary testing

--- solutions/p347TopKFrequentElements.py
+++ solutions/p347TopKFrequentElements.py
@@ -4,6 +4,3 @@ class Solution(object):
         freq = collections.Counter(nums)
         s = sorted(freq.items(), key=lambda x: x[1], reverse=True)
         return list(i for i,c in s[:k])
-
-s = Solution()
-print(s.topKFrequent([1,1,1,1,2,2,2,3,3,3,3,3,3], 2))
